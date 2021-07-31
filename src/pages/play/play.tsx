eb7a8353580164a2dd382c40d9b72991eada692a
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-31 18:13:58 -0400

    add scenes to game config

--- src/pages/play/play.tsx
+++ src/pages/play/play.tsx
@@ -1,9 +1,17 @@
 import PhaserGame from "views/phaser-game";
+import create from "scenes/create";
+import preload from "scenes/preload";
+import update from "scenes/update";
 
 export const Play = (): JSX.Element => {
   return (
     <PhaserGame
-      config={{ backgroundColor: "000000", height: 300, width: 400 }}
+      config={{
+        backgroundColor: "000000",
+        height: 300,
+        scene: { create: create, preload: preload, update: update },
+        width: 400,
+      }}
     />
   );
 };
