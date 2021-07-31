560ad8505ac40d6b45a3b22d027c7c3aa6afb0f4
Ben Rosen <benjamin.p.rosen@gmail.com>
2021-07-31 15:21:31 -0400

    add page routing in App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import ErrorPage from "pages/error";
+import HomePage from "pages/home";
+import LevelsPage from "pages/levels";
+import PlayPage from "pages/play";
 
 export const App = (): JSX.Element => {
-  return <div>oubliette</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/levels">
+          <LevelsPage />
+        </Route>
+        <Route path="/play">
+          <PlayPage />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
